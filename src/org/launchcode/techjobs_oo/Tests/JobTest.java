4e23c9a88d46254df3023cc2368a97f4aba75694
Vicki20rous <vic86rous@gmail.com>
2021-10-26 07:59:44 -0500

    added test package and class

---
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -0,0 +1,19 @@
+package org.launchcode.techjobs_oo.Tests;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class JobTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testSettingJobId() {
+
+    }
+}
